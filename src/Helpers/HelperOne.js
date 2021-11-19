05f35cb4cb581b14a2c27080e1a9c8b839ea19e3
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2021-11-19 15:24:52 +0300

    helper files

---
+++ src/Helpers/HelperOne.js
@@ -0,0 +1,17 @@
+
+fetch('https://regres.in/api/users', 
+        {
+          method: 'POST',
+          headers: {
+                'Content-Type': 'application/json'
+                    },
+          body: JSON.stringify({
+              name: 'User 1'
+                    })
+        }            
+      )            
+  .then(res => {
+        return res.json()
+    })
+  -then (data => console.log(data))
+    .catch(error => console.log('ERROR'))
